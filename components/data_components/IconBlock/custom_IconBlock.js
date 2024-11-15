49f3662cc82442fc9b117c5e3f43c2791cb7857d
p20199 <p20199@unipi.gr>
2024-11-15 12:22:49 +0200

    icon block get in touch

--- components/data_components/IconBlock/custom_IconBlock.js
+++ components/data_components/IconBlock/custom_IconBlock.js
@@ -1,13 +1,68 @@
-// Import the HTML template
-//import CustomIconBlockTemplate from './custom_IconBlock.html';
 
 const CustomIconBlockTemplate = {
-    props: {
-      model: Object,
-    },
+  props: {
+    model: Object
+  },
+  data() {
+      return {
+          backgroundColor: this.model.backgroundColor,
+          textcolor: this.model.textColor,
+          bgClass: null,
+          pattern: [],
+          fullWidth: this.model.blockExtended ? this.model.blockExtended : false,
+      }
+  },
+  mounted() {
+      this.bgClass = `background-color: ${this.backgroundColor};color:${this.textcolor}`;
+  },
+  methods: {
+      getAlignmentClass(alignment) {
+          switch (alignment) {
+              case 1:
+                  return 'justify-content-start';
+              case 2:
+                  return 'justify-content-center';
+              case 3:
+                  return 'justify-content-end';
+              default:
+                  return '';
+          }
+      },
+      getAlt(iconblock) {
+          if (!iconblock)
+              return null;
+          if (iconblock?.alt === null || iconblock?.alt === undefined || iconblock?.alt === '') {
+              return "icon";
+          }
+          return iconblock.image.alt;
+      },
+      getImage(iconblock) {
+          if (!iconblock)
+              return null;
+          if (iconblock?.image === null || iconblock?.image === undefined) {
+              return "/images/no_image.png";
+          }
+          return iconblock.image.link;
+      }
+  },
+  created() {
+      this.pattern = [12];
+      if (this.model.columns === 1) {
+          this.pattern = [12];
+      }
+      if (this.model.columns === 2) {
+          this.pattern = [6, 6];
+      }
+      if (this.model.columns === 3) {
+          this.pattern = [4, 4, 4];
+      }
+      if (this.model.columns === 4) {
+          this.pattern = [3, 3, 3, 3];
+      }
+  }
 }
 
-app.component('iconblockcustom_IconBlock', 
-  {  extends: CustomIconBlockTemplate,
-  template: '#iconblockcustom_IconBlock'
+app.component('iconblockcustom_iconblock', {  
+  extends: CustomIconBlockTemplate,
+  template: '#iconblockcustom_iconblock'
 });
